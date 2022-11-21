9213ae4dea1098aeefae01ec9e672652ac1af987
ismat27 <adegbiteismail27@gmail.com>
2022-11-21 12:44:25 -0800

    updates

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
-import ProductsList from './components/ProductsList';
+// import ProductsList from './components/ProductsList';
+import NewProduct from './components/NewProduct';
 
 // "proxy": "http://localhost:8000", 
 
 function App() {
   return (
     <div className="App">
-      <ProductsList />
+      {/* <ProductsList /> */}
+      <NewProduct />
     </div>
   );
 }
