fa5346a63cac7dcd0bc422240aab2504dda2419f
ismat27 <adegbiteismail27@gmail.com>
2022-12-25 09:27:55 -0800

    updates

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import ProductsList from './components/ProductsList';
 import NewProduct from './components/NewProduct';
+import Header from './components/Header';
 
 function App() {
   return (
     <Router>
+      <Header />
       <Routes>
         <Route path='/' element={<ProductsList />} />
         <Route path='/products' element={<NewProduct />} />
