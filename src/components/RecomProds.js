19bf7600f0c2024e7a9bef266d464c0a880be55b
ismat27 <adegbiteismail27@gmail.com>
2022-12-27 05:48:20 -0800

    updates

--- src/components/RecomProds.js
+++ src/components/RecomProds.js
@@ -1,54 +1,66 @@
 import React from 'react'
 import styled from 'styled-components'
 import RecomProdCard from './RecomProdCard'
+import CompHeader from './CompHeader'
 
 const RecomProds = () => {
   return (
     <Wrapper>
-        <RecomProdCard 
-            data={{}}
+        <CompHeader 
+            title={'recommended'}
         />
-        <RecomProdCard 
-            data={{}}
-        />
-        <RecomProdCard 
-            data={{}}
-        />
-        <RecomProdCard 
-            data={{}}
-        />
-        <RecomProdCard 
-            data={{}}
-        />
-        <RecomProdCard 
-            data={{}}
-        />
+        <div className='products'>
+            <RecomProdCard 
+                data={{}}
+            />
+            <RecomProdCard 
+                data={{}}
+            />
+            <RecomProdCard 
+                data={{}}
+            />
+            <RecomProdCard 
+                data={{}}
+            />
+            <RecomProdCard 
+                data={{}}
+            />
+            <RecomProdCard 
+                data={{}}
+            />
+        </div>
     </Wrapper>
   )
 }
 
 const Wrapper = styled.div`
-display: grid;
-gap: 1rem;
-justify-content: center;
-background-color: var(--white);
-box-shadow: 1px 1px 10px rgba(0, 0, 0, 0.05);
-padding: .7rem 1.34rem;
+.products {
+    display: grid;
+    gap: 1rem;
+    justify-content: center;
+    background-color: var(--white);
+    box-shadow: 1px 1px 10px rgba(0, 0, 0, 0.05);
+    padding: .7rem 1.34rem;
+}
 
 @media (min-width: 576px) {
     
 }
 @media (min-width: 768px) {
-    grid-template-columns: repeat(2, auto);
-    justify-content: stretch;
-    row-gap: 2rem;
-    column-gap: .5rem;
-    padding: 1rem;
+    .products {
+        grid-template-columns: repeat(2, auto);
+        justify-content: stretch;
+        row-gap: 2rem;
+        column-gap: .5rem;
+        padding: 1rem;
+    }
 }
 @media (min-width: 992px) {
-    grid-template-columns: repeat(3, auto);
-    column-gap: .5rem;
-    padding: .7rem 1rem;
+    .products {
+        grid-template-columns: repeat(3, auto);
+        column-gap: .5rem;
+        padding: .7rem 1rem;
+    }
 }
 `
 
