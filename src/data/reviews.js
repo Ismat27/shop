71b1adf62d67f7c3c2a5f7ea971e4d36b72beb01
ismat27 <adegbiteismail27@gmail.com>
2022-12-28 11:50:32 -0800

    placeholder for clients reviews

---
+++ src/data/reviews.js
@@ -0,0 +1,44 @@
+export const reviews = [
+    {
+        id: 1,
+        name: 'ismail adegbite',
+        text: 'Best platform to shop online for quality and affordable products',
+        image: '',
+        rating: 4.8,
+    },
+    {
+        id: 2,
+        name: 'fahim steven',
+        text: 'Good customer service that cannot be found elsewhere. I will not hesitate to refer this platform for buying good quality gadgets',
+        image: '',
+        rating: 4.5,
+    },
+    {
+        id: 3,
+        name: 'bolu olaiya',
+        text: 'A trial turned me to a regular customer. My number one shop for getting quality and reliable gadgets for my mobile devices',
+        image: '',
+        rating: 4.6,
+    },
+    {
+        id: 4,
+        name: 'muniru sobayo',
+        text: '',
+        image: '',
+        rating: 4.7,
+    },
+    {
+        id: 5,
+        name: 'steven aina',
+        text: 'The only platform to get quality products you cannot get elsewhere',
+        image: '',
+        rating: 4.4,
+    },
+    {
+        id: 6,
+        name: 'yisa yahyah',
+        text: 'Their delivery is timely and they respond well to customer enquiries and complaints',
+        image: '',
+        rating: 4.0,
+    },
+]
