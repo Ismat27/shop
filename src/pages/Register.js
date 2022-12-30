b3cfcb08eaee9cd32d62f6547efab9cf33b73dc2
ismat27 <adegbiteismail27@gmail.com>
2022-12-30 11:27:41 -0800

    set up file for registration page

---
+++ src/pages/Register.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Register = () => {
+  return (
+    <Wrapper>
+        <div>
+            <form>
+                
+            </form>
+        </div>
+    </Wrapper>
+  )
+}
+
+const Wrapper = styled.section`
+`
+
+export default Register
