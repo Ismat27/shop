e7bfeefbb1caaa31eb19694d654caa6d742f8525
ismat27 <adegbiteismail27@gmail.com>
2023-01-02 05:52:26 -0800

    update component for displaying products

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import ProductsList from './components/ProductsList';
 import NewProduct from './components/NewProduct';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './pages/Home';
 import Register from './pages/Register';
 import Login from './pages/Login';
+import Shop from './pages/Shop';
 import Cart from './pages/Cart';
 import Checkout from './pages/Checkout';
 
@@ -16,7 +16,7 @@ function App() {
       <Header />
       <Routes>
         <Route path='/' element={<Home />} />
-        <Route path='/shop' element={<ProductsList />} />
+        <Route path='/shop' element={<Shop />} />
         <Route path='/products' element={<NewProduct />} />
         <Route path='/register' element={<Register />} />
         <Route path='/login' element={<Login />} />
