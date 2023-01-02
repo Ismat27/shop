7afdb3d6784eefe824f41c05b87b8d2bae69653b
ismat27 <adegbiteismail27@gmail.com>
2023-01-02 05:05:49 -0800

    checkout page

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Home from './pages/Home';
 import Register from './pages/Register';
 import Login from './pages/Login';
 import Cart from './pages/Cart';
+import Checkout from './pages/Checkout';
 
 function App() {
   return (
@@ -20,6 +21,7 @@ function App() {
         <Route path='/register' element={<Register />} />
         <Route path='/login' element={<Login />} />
         <Route path='/cart' element={<Cart />} />
+        <Route path='/checkout' element={<Checkout />} />
       </Routes>
       <Footer />
     </Router>
