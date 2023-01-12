78256e8893c0e58b78a5528aa91ef464d61bbf78
ismat27 <adegbiteismail27@gmail.com>
2023-01-12 11:39:39 -0800

    initiate context for fetching data

--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import AuthContext from './contexts/AuthContext';
+import ProductContext from './contexts/ProductContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <AuthContext>
-      <App />
+      <ProductContext>
+        <App />
+      </ProductContext>
     </AuthContext>
   </React.StrictMode>
 );
