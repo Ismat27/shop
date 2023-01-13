be0f8b325fac0b8e990518fccd66991707318578
ismat27 <adegbiteismail27@gmail.com>
2023-01-13 05:53:14 -0800

    added variable and function for handling single product fetching

--- src/contexts/ProductReducer.js
+++ src/contexts/ProductReducer.js
@@ -1,6 +1,9 @@
 const FETCH_PRODUCTS_FAILED = 'FETCH_FAILED';
 const FETCH_PRODUCTS_START = 'FETCH_PRODUCTS_LOADING';
 const FETCH_PRODUCTS_SUCCESS = 'FETCH_PRODUCTS_SUCCESS'
+const FETCH_SINGLE_PRODUCT_START = 'FETCH_SINGLE_PRODUCT_LOADING';
+const FETCH_SINGLE_PRODUCT_SUCCESS = 'FETCH_SINGLE_PRODUCT_SUCCESS'
+const FETCH_SINGLE_PRODUCT_FAILED = 'FETCH_SINGLE_PRODUCT_FAILED';
 
 const reducer = (state, action) => {
     if (action.type === FETCH_PRODUCTS_START) {
@@ -27,10 +30,39 @@ const reducer = (state, action) => {
             products_loading_error: true,
         }
     }
+    // Single product fetching
+    if (action.type === FETCH_SINGLE_PRODUCT_START) {
+        return {
+            ...state,
+            single_product_loading: false,
+            single_product_loading_error: false,
+        }
+    }
+    if (action.type === FETCH_SINGLE_PRODUCT_SUCCESS) {
+        const data = action.payload
+        return {
+            ...state,
+            single_product: data,
+            single_product_loading: false,
+            single_product_loading_error: false,
+        }
+    }
+    if (action.type === FETCH_SINGLE_PRODUCT_FAILED) {
+        return {
+            ...state,
+            single_product_loading: false,
+            single_product_loading_error: true,
+        }
+    }
+    throw new Error('Unknown action')
 }
 
 export {
-    FETCH_PRODUCTS_FAILED, FETCH_PRODUCTS_START,
-    FETCH_PRODUCTS_SUCCESS
+    FETCH_PRODUCTS_FAILED, 
+    FETCH_PRODUCTS_START,
+    FETCH_PRODUCTS_SUCCESS,
+    FETCH_SINGLE_PRODUCT_START,
+    FETCH_SINGLE_PRODUCT_SUCCESS,
+    FETCH_SINGLE_PRODUCT_FAILED
 }
 export default reducer
